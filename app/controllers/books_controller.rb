e197a3a419fa855b576375acb35da5b3917cfc96
LetiTran <leticiastran@gmail.com>
2018-03-20 10:14:36 -0700

    Created the DatabBase files.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,22 @@
 class BooksController < ApplicationController
+
+  #Simulate a data base:
+  BOOKS_LIST = [
+    {id: 1, title: "1984", author: "George Orwell", descr: "Ut arabica dark, instant fair trade flavour et wings. Filter, coffee iced, aromatic, coffee in to go doppio mazagran filter. Single origin aftertaste robusta, aged, espresso, milk to go cappuccino sugar robusta."},
+    {id: 2,title: "Pride & Prejudice", author: "Jane Austin", descr: "Crema variety grounds decaffeinated chicory decaffeinated organic affogato. Iced, caramelization wings, java grinder aftertaste cultivar arabica cup body. Fair trade and saucer, americano fair trade, aromatic, cup, mug as that half and half dripper."},
+    {id: 3,title: "Harry Potter", author: "Jk Rowling", descr: "Froth to go crema organic, bar medium filter brewed steamed. Shop lungo redeye, trifecta, redeye and roast shop variety. Roast, eu viennese, french press, at and americano body dripper shop french press"},
+    {id: 4,title: "Harry Poodr", author: "Sandy Metz", descr: "That, wings frappuccino shop foam, roast, kopi-luwak grinder variety beans breve caramelization. Carajillo steamed, carajillo, kopi-luwak robust, doppio, irish, sugar white redeye eu flavour. Rich cortado, caffeine cinnamon at in seasonal."}
+  ]
+
+  def index
+    @books = BOOKS_LIST
+  end
+
   def show
+    id = params[:id]
+    @book = BOOKS_LIST.find do |book|
+      book[:id] == id.to_i
+    end
   end
 
   def new
@@ -17,6 +34,5 @@ class BooksController < ApplicationController
   def destroy
   end
 
-  def index
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
 
+  get '/books/new', to: 'books#new', as: 'new_book'
+  # get 'books/search/'
+  get '/books/:id', to: 'books#show', as: 'book'
   get '/books', to: 'books#index', as: 'books'
-
-   get '/books/new', to: 'books#new', as: 'new_book'
-
-   post '/books', to: 'books#create'
-
-   get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
-
-   get '/books/:id', to: 'books#show', as: 'book'
-
-   patch '/bookd/:id', to: 'books#update'
-
-   delete '/books/:id', to: 'books#destroy'
-
+  post '/books', to: 'books#create'
+  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+  patch '/books/:id', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# as: the name u want the path to have
