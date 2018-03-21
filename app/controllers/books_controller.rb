e974a4aa068d71957f853e1e15ca280d1538eaac
LetiTran <leticiastran@gmail.com>
2018-03-21 14:06:58 -0700

    Added else to the redirect_to on controller#create in case saving the new book does not work - it will stay on same page and show the form again.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,15 +14,19 @@ class BooksController < ApplicationController
     # This .new action sends a request to the router to return a form for the new book to the user.
   end
 
+  # NEW and CREATE are two DIFFERENT CYCLES. The new method is calling the create method when u hit the submitt button.
+
   def create
     # This will actually create the new book:
     book = Book.new
     book.title = params[:book][:title]
     book.author = params[:book][:author]
     book.description = params[:book][:description]
-    redirect_to books_path if book.save # .save returns T or F
+    book.save ? (redirect_to books_path) : (render :new)
+    # .save returns T or F
+    #if .save does NOT work, stay on this page and show the form again.
   end
-  # delete the create.html.erb file! because you dont need it anymore!
+  # DELETE the create.html.erb file! because it is no longer rendering its own view because it is redirecting to a different action.... so, you dont need it anymore!
 
   def edit
   end
