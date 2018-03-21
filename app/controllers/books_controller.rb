b0168a8d4d7244d381d6eeb467497ba29b9a519a
LetiTran <leticiastran@gmail.com>
2018-03-21 14:00:13 -0700

    Deleted the create.html.erb  file because controller#create now is redirecting to a different thing instead of rendering its own view.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,18 +11,18 @@ class BooksController < ApplicationController
 
   def new
     @book = Book.new
-    # this instanciates a new book and leave all the fields blank
+    # This .new action sends a request to the router to return a form for the new book to the user.
   end
 
   def create
-    @book = Book.new
-    @book.title = params[:book][:title]
-    @book.author = params[:book][:author]
-    @book.description = params[:book][:description]
-    redirect_to books_path if @book.save
-    # .save returns T or F
-
+    # This will actually create the new book:
+    book = Book.new
+    book.title = params[:book][:title]
+    book.author = params[:book][:author]
+    book.description = params[:book][:description]
+    redirect_to books_path if book.save # .save returns T or F
   end
+  # delete the create.html.erb file! because you dont need it anymore!
 
   def edit
   end
