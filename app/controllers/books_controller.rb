9f4fccb3c0e407df39f539acc3e88e35d5685923
LetiTran <leticiastran@gmail.com>
2018-03-22 10:05:12 -0700

    Added controller#edit and controller#update method and changed their respective view files.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
   end
 
   def new
+    # does a GET request:
     @book = Book.new
     # This .new action sends a request to the router to return a form for the new book to the user.
   end
@@ -17,21 +18,31 @@ class BooksController < ApplicationController
   # NEW and CREATE are two DIFFERENT CYCLES. The new method is calling the create method when u hit the submitt button.
 
   def create
+    # does  POST request:
     # This will actually create the new book:
     book = Book.new
     book.title = params[:book][:title]
     book.author = params[:book][:author]
     book.description = params[:book][:description]
+
+    # does a GET request:
     book.save ? (redirect_to books_path) : (render :new)
     # .save returns T or F
     #if .save does NOT work, stay on this page and show the form again.
+
   end
   # DELETE the create.html.erb file! because it is no longer rendering its own view because it is redirecting to a different action.... so, you dont need it anymore!
 
   def edit
+    Book.find_by(id: params[:id]) ? @book = Book.find_by(id: params[:id]) : new
   end
 
   def update
+    book = Book.find_by(id: params[:id])
+    if !@book.nil?
+      #need to check both editi and update for the existency of the book because there are ways of editing a form without using the broweser.
+      book.update(title: params[:book][:title], author: params[:book][:author], description: params[:book][:description] ) ? (redirect_to book_path) : (render :edit)
+    end
   end
 
   def destroy
