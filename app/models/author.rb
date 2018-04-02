b7c7654b0b8d5b050fec142d9e4e2897ac27633d
LetiTran <leticiastran@gmail.com>
2018-04-02 16:24:16 -0700

    Change seeds.rb file(hardoced some info there for authors and books) and reseted the database using the seeds file.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,20 @@
 class Author < ApplicationRecord
   has_many :books
+
+
+  # Learning about model logic - textbook example:
+  # (to add a field with the info 'first published year')
+
+  #  in this file:
+  # def first_published
+  #   books_with_year = self.books.where.not(publication_date: nil)
+  #   first_book = books_with_year.order(publication_date: :asc).first
+  #   return first_book.publication_date
+  # end
+
+  # Then in the show.html.erb file put this:
+  # <p>
+  # The author <%= @book.author.name %> was first published in the year <%= @book.author.first_published %>
+  # </p>
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# ______________________________
+# Create authors:
+
+author_list = [
+  {
+    name: "J.K. Rowling"
+  },
+  {
+    name: "Stephen King"
+  },
+  {
+    name: "William Shakesphere"
+  },
+]
+
+author_list.each do |author|
+  worked = Author.create(author)
+  # to make sure it worked:
+  puts "#{author[:name]} has ID: #{worked}"
+end
+
+
+# ______________________________
+# Create books:
+
+books_list = [
+  {
+    title: "Harry Potter",
+    author: Author.find(1),
+    description: "A book"
+  },
+  {
+    title: "Scary",
+    author: Author.find(2),
+    description: "Another book"
+  },
+  {
+    title: "A love story",
+    author: Author.find(3),
+    description: "Another second book"
+  },
+]
+
+books_list.each do |book|
+  worked = Book.create(title: book[:title], author: book[:author], description: book[:description])
+  # to make sure it worked:
+  puts "#{book[:title]} has ID: #{worked}, has author: #{book[:author]}"
+end
